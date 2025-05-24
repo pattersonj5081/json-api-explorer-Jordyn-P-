ad86af045d3d73461be48aed0466c870190222b3
pattersonj5081 <150544502+pattersonj5081@users.noreply.github.com>
2025-05-24 10:37:11 -0500

    code that we completed in class together as an IA group for JSON 6

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 // so much empty
+const postList = document.getElementById("postList"); 
+
+const renderPosts = (posts)=> {
+    postList.innerHTML = ""; 
+    posts.forEach((post) =>{
+        const postElement = document.createElement("div")
+        postElement.innerHTML = `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <hr/>
+        `;
+        postList.appendChild(postElement); 
+
+    })
+
+}
+fetch ("https://jsonplaceholder.typicode.com/posts?_limit=5")
+.then(function(response){
+    const jsonResponse = response.json()
+    return jsonResopnse
+}).then(function(data){
+    renderPost(data);
+})
+.catch((error)=> console.error("Error fething posts:", error))
